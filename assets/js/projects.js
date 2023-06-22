613f85ae8c21ab9593236798d2d473b40a750b89
ImCodist <50346006+ImCodist@users.noreply.github.com>
2023-06-22 02:19:03 -0400

    Tags Box

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -4,6 +4,28 @@ var unlocked = false;
 var sound = new Audio("/assets/sounds/secret.ogg");
 var soundblip = new Audio("/assets/sounds/secret_start.ogg");
 
+function updateProjects() {
+    var projects = document.querySelectorAll(".projects li");
+    projects.forEach((project) => {
+        if (project.classList.contains("badproject") && !unlocked) return;
+        project.classList.add("invisible");
+
+        var WORTHY = false;
+        
+        var selected_categories = $(".list_page_input #tags").select2("data");
+        if (selected_categories.length <= 0) WORTHY = true;
+
+        var categories = project.getAttribute("categories").split(" ");
+        selected_categories.forEach((category) => {
+            if (categories.includes(category.element.getAttribute("value"))) {
+                WORTHY = true;
+            }
+        })
+
+        if (WORTHY) project.classList.remove("invisible");
+    });
+}
+
 function stepCheck(neededSteps) {
     if (neededSteps.includes(step)) {
         step += 1;
@@ -49,4 +71,16 @@ document.addEventListener('keydown', function(event) {
             soundblip.play();
         }
     }
+});
+
+$(document).ready(function() {
+    var tagsBox = $(".list_page_input #tags");
+    tagsBox.select2({
+        placeholder: "Tags",
+        allowClear: true
+    });
+    tagsBox.on("select2:select", updateProjects);
+    tagsBox.on("select2:unselect", updateProjects);
+
+    updateProjects();
 });
